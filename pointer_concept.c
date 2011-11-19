696979fdf62762f8df0e022b70da15eab9457516
anish kumar <anish198519851985@gmail.com>
2011-11-19 19:57:38 +0900

    added double pointer concept of typecasting
    Signed-off-by: anish kumar <anish198519851985@gmail.com>

--- pointer_concept.c
+++ pointer_concept.c
@@ -6,7 +6,7 @@ struct a{
 
 void show_doublepointer(void);
 void foo(int (*b)[3]);
-
+void character_doublepointer_to_const_doublepointer(void);
 main()
 {
 	int a[] = {0};
@@ -20,6 +20,7 @@ main()
 	printf("%d %d %d \n",*p[0],*p[1],*p[2]);
 	foo(&b[0]);
 	show_doublepointer();
+	character_doublepointer_to_const_doublepointer();
 }
 
 void foo(int (*b)[3])
@@ -49,3 +50,15 @@ void show_doublepointer(void)
 	for(i = 0;i < sizeof t/sizeof *t && t[i] != NULL;i++)
 		printf("%s\n",t[i]);
 }
+
+/*http://c-faq.com/ansi/constmismatch.html*/
+void character_doublepointer_to_const_doublepointer(void)
+{
+const char i = 'a';
+char *p1;                      
+const char **p2 = (const char **)&p1;
+*p2 = &i;
+*p1 = 'b';
+printf("%c\n", i); 
+}
+
