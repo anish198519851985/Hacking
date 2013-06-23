7831445791cab469d436e53b4e774658d93c6d34
anish kumar <anish198519851985@gmail.com>
2013-06-23 23:15:47 +0530

    added the dp problem
    Signed-off-by: anish kumar <anish198519851985@gmail.com>

--- min_coin_change.c
+++ min_coin_change.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int comb[] = {1, 2, 5};
-
+#define TARGET 23
+#define BIG_VALUE 9999
+int comb[] = {3, 5};
 int min(int x, int y, int z)
 {
 	int a = x<y?x:y;
@@ -8,18 +9,44 @@ int min(int x, int y, int z)
 	return b;
 }
 
+int min_(int x, int y)
+{
+	int a = x<y?x:y;
+	return a;
+}
+
 int min_change(int x)
 {
+	printf("%s\n", __func__);
 	if(x <= 0)
 		return 0;
 	if(x == 0)
 		return 0;
 	else
-		return 1 + min(min_change(x-1), min_change(x-2), min_change(x-5));
+		return 1 + min_(min_change(x-3), min_change(x-5));
+}
+
+int result[100];
+int m_c_dp(int sum)
+{
+	int i, j;
+
+	result[0]=0;
+	for(i=1;i<=sum;i++) {
+		for(j=0;j<sizeof(comb)/sizeof(comb[0]);j++) {
+			if(i >= comb[j] && result[i-comb[j]]+1 < result[i]) {
+				result[i] = 1 + result[i-comb[j]];
+			}
+		}
+	}
+	return result[sum];
 }
 
 int main()
 {
-	printf("%d\n", min_change(21));
+	int i;
+	for(i=0;i<100;i++)
+		result[i] = BIG_VALUE;
+	printf("%d\n", m_c_dp(TARGET));
 	return 0;
 } 
