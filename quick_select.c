6afedd71600d5245647ef6690bfb43fbe127200b
anish kumar <anish198519851985@gmail.com>
2013-06-30 14:55:41 +0530

    modified quick_select.c
    anish198519851985@gmail.com

--- quick_select.c
+++ quick_select.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 void swap(int a[], int  i, int j)
 {
 	int temp = a[i];
@@ -13,11 +14,13 @@ int partition(int a[], int left, int right)
 	swap(a, pivot, right);
 	right--;
 	while(left < right){
-		while(a[left] > element)
+		while(a[left] <= element && left <= right)
 			left++;
-		while(a[right] < element)
+		while(a[right] > element)
 			right--;
-		if(left < right)
+		if(left >= right)
+			break;
+		else
 			swap(a, left, right);
 	}
 	swap(a, left, copy);
@@ -26,24 +29,27 @@ int partition(int a[], int left, int right)
 
 int q_s(int a[], int left, int right, int k)
 {
+	if(left == right)
+		return a[left];
 	if(left < right) {
+		int i;
+		printf("left %d right %d\n", left, right);
 		int pivot = partition(a, left, right);
-		printf("%d %d %d %d\n", left, right, pivot, k);
-		if(pivot == k) {
-			printf("found it\n");
-			return a[pivot];
-		}
-		if(pivot < k)
-			q_s(a, pivot+1, right, k);
-		else
+		printf("pivot %d\n", pivot);
+		int newPivotDistance = pivot - left + 1;
+		printf("k is %d pivot distance %d\n", k, newPivotDistance);
+		if(newPivotDistance == k) return a[pivot];
+		if(newPivotDistance > k)
 			q_s(a, left, pivot-1, k);
+		else
+			q_s(a, pivot+1, right, k-newPivotDistance);
 	}
 }
 
 int main()
 {
-	int i;
-	int a[] = {2, -4, 5, 6, 0, 7, -1, 10, 9};
-	int k = 2; //if you want to find out 1 largest then give 0
+	int a[] = {50,  6, 5, 7, 40,  10, 10, 0, 5, 19,10};
+	int k;
+	scanf("%d", &k);
 	printf("%d\n", q_s(a, 0, sizeof(a)/sizeof(a[0]) -1, k));
 }
