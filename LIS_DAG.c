57675a079d411a73fd66f196b2338eb84272a8ce
anish kumar <anish198519851985@gmail.com>
2013-07-07 23:03:31 +0530

    LIS changed
    Signed-off-by: anish kumar <anish198519851985@gmail.com>

--- LIS_DAG.c
+++ LIS_DAG.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define SIZE(x) (sizeof(x)/sizeof(x[0]))
+#define SIZE(x) sizeof(x)/sizeof(x[0])
+
 int maxi(int a,int b)
 {
 	return a>b?a:b;
 }
-
-int a[] = {10,5,2,5,2,13,17,15,16,19,20,21};
+int a[] = {857, -838, 84, 763, 245, 799, -457, 989, -349, 174, -151, -392, -621, -625, 507, -101, -921, -367, 734, 661};
 
 /* LIS using constructing a DAG */
 void DAG()
 {
 	int i, j, max=0,index;
-	int d[100] = {0};
-	int dag[100][100] = {0};
-	int prev[100] = {0};
+	int d[100];
+	int dag[100][100];
+	int prev[100];
 
 	for(i=0;i<SIZE(a);i++) {
 		d[i]=1;prev[i]=0;
@@ -27,12 +30,13 @@ void DAG()
 			if(a[i] <= a[j])
 				dag[i][j] = 1;
 
-	d[0]=1;
 	for(i=0;i<SIZE(a);i++)
 		for(j=0;j<SIZE(a);j++) {
 			if(dag[i][j] == 1) {
-				d[j] = maxi(d[j], 1+d[i]);
-				prev[j] = i;
+				if(d[j] <= 1+d[i]) {
+					d[j] = 1+d[i];
+					prev[j] = i;
+				}
 			}
 		}
 
@@ -44,6 +48,7 @@ void DAG()
 	printf("max index %d and greatest total elements %d\n", index, max);
 	printf("%d\n", a[index]);
 	while(--max){
+		printf("index %d\n", index);
 		index = prev[index];
 		printf("%d\n", a[index]);
 	}
