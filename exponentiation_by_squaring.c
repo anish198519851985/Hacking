e8d2304d8a0f3ab58440aa3bdeecc8df87915743
anish kumar <anish198519851985@gmail.com>
2014-06-22 23:19:13 -0700

    Program was wrong added the changes to make it right
    Remember it is multiplication by squaring.
    
    Signed-off-by: anish kumar <anish198519851985@gmail.com>

--- exponentiation_by_squaring.c
+++ exponentiation_by_squaring.c
@@ -1,35 +1,23 @@
 /* 
 x^n = {
 	1 if n==0
-	1/(x^n) if n < 0
-	x.x^(n-1/2)*2 if n is odd
-	x.x^(n/2)*2 if n is even
+	x*x^2*x^(n-1/2) if n is odd
+	(x^2)^(n/2) if n is even
       }
-def power(x,n)
-   result = 1
-   while n.nonzero?
-   if n[0].nonzero?
-   result *= x
-   n -= 1
-   end
-   x *= x
-   n /= 2
-   end
-   return result
-   end
- */
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int multiply_by_exponent(int number, int power, int result)
+int multiply_by_squaring(int number, int power)
 {
 	if(power ==0)
-		return result;
+		return 1;
+	if(power ==1)
+		return number;
 	if(power%2 == 0) {
-		multiply_by_exponent(number*number, power/2, result);
+		return multiply_by_squaring(number*number, power/2);
 	} else {
-		--power;
-		multiply_by_exponent(number*number, power/2, result*number);
+		return number*multiply_by_squaring(number*number, (power-1)/2);
 	}
 }
 
@@ -40,6 +28,6 @@ int main()
 	printf("please enter the number and follow it up with the exponent\n");
 	scanf("%d %d", &number, &power);
 
-	printf("%d\n", multiply_by_exponent(number, power, 1));
+	printf("%d\n", multiply_by_squaring(number, power));
 	return 0;
 }
